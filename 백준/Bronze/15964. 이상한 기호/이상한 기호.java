c403d1510ee8575035471e8ea3d3030c17314e9e
alabacannon <kim3210542@gmail.com>
2025-08-17 01:29:59 +0900

    [Bronze V] Title: 이상한 기호, Time: 168 ms, Memory: 17672 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,14 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		long ans = (a + b) * (a - b);
+		System.out.println(ans);
+	}
+}
