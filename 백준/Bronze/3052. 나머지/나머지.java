0b3bf938d7eaefd4cc970d411fda3c6adb945293
alabacannon <kim3210542@gmail.com>
2025-08-17 00:54:25 +0900

    [Bronze II] Title: 나머지, Time: 168 ms, Memory: 17548 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -4,23 +4,22 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-//		System.setIn(new FileInputStream("data/input.txt"));
-		Scanner sc = new Scanner(System.in);
-		int n = 10;
-		int count = 0;
-		ArrayList<Integer> arr = new ArrayList<>();
-		for (int i = 0; i < n; i++) {
-			int temp = sc.nextInt()%42;
-			if (arr.stream()
-					.filter(a -> a == temp)
-					.findAny()
-					.orElse(-1)
-					== -1) {
-				arr.add(temp);
-				count++;
-			}
-		}
-		System.out.println(count);
-	}
+    public static void main(String[] args) throws IOException {
+//        System.setIn(new FileInputStream("data/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        int n = 10;
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            int temp = sc.nextInt() % 42;
+            
+            // ArrayList에 포함되어 있지 않은 경우에만 추가
+            if (!arr.contains(temp)) {
+                arr.add(temp);
+            }
+        }
+        
+        // 최종적으로 리스트에 담긴 요소의 개수(고유한 값의 개수)를 출력
+        System.out.println(arr.size());
+    }
 }
