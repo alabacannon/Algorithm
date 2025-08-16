46b045b671b9b77a246c7678f09266eb630e4606
alabacannon <kim3210542@gmail.com>
2025-08-17 01:13:37 +0900

    [Bronze V] Title: ??!, Time: 168 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		String id = sc.next();
+		System.out.println(id + "??!");
+		
+	}
+}
