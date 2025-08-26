3b3ae6d8d7565f9156ceba77a70ff3109f25afa3
alabacannon <kim3210542@gmail.com>
2025-08-26 17:31:38 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 168 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,14 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+//		System.setIn(new FileInputStream("data/input.txt"));
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(n);
+		System.out.println(1);
+	}	
+	
+}
