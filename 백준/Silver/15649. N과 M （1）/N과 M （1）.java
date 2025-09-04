28a3e1a330744188103185887ee0468637988791
alabacannon <kim3210542@gmail.com>
2025-09-04 09:15:06 +0900

    [Silver III] Title: N과 M (1), Time: 344 ms, Memory: 38000 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -6,22 +6,25 @@ public class Main {
 	static int n;
 	static int m;
 	static int[] sel;
+	static StringBuilder sb;
 	public static void main(String[] args) throws IOException {
 		//System.setIn(new FileInputStream("data/input.txt"));
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		m = sc.nextInt();
+		sb = new StringBuilder();
 		sel = new int[m];
 		permutation(0,0);
+		System.out.println(sb.toString());
 		
 	}
 	private static void permutation(int cnt, int visited) {
 		if (cnt == m) {
 			for (int num : sel) {
-				System.out.print(num + " ");
+				sb.append(num + " ");
 			}
-			System.out.println();
-            return;
+			sb.append("\n");
+			return;
 		}
 		for (int i = 1; i <= n; i++) {
 			if ((visited & (1<<i)) != 0) continue; 	
