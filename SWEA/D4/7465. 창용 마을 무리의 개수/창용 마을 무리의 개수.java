d9baf4c4178efa01f2010f02059533b23c2d4f57
alabacannon <kim3210542@gmail.com>
2025-09-09 14:13:28 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 101 ms, Memory: 28,160 KB -BaekjoonHub

---
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int n,m;
+	static int[] parent, rank;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new FileReader("data/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		StringTokenizer st;
+		int t = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= t; test_case++) {
+			Set<Integer> set = new HashSet<>();
+			sb.append("#").append(test_case).append(" ");
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			parent = new int[n+1];
+			rank = new int[n+1];
+			for (int i = 1; i <= n; i++) {
+				parent[i] = i;
+				rank[i] = 1;
+			}
+			for (int k = 0; k < m; k++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				int p1 = find(a);
+				int p2 = find(b);
+				if (p1 < p2) {
+					parent[p2] = p1;
+				} else {
+					parent[p1] = p2;
+				}
+			}
+			for (int i = 1; i <= n; i++) {
+				find(i);
+				set.add(parent[i]);
+			}
+			sb.append(set.size()).append("\n");
+		}
+		System.out.println(sb.toString());
+		br.close();
+	}
+	
+	private static int find(int a) {
+		if (a == parent[a]) return a; 			
+		return parent[a] = find(parent[a]);
+	}
+}
