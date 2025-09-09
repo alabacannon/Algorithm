875df7bd964b37c8345a1459b9ccba1ba2784937
alabacannon <kim3210542@gmail.com>
2025-09-09 15:44:33 +0900

    [Gold III] Title: 별자리 만들기, Time: 244 ms, Memory: 19160 KB -BaekjoonHub

--- 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.java
@@ -1,79 +1,88 @@
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
-	static class Node implements Comparable<Node>{
-		int v1;
-		int v2;
-		double dist;
-		public Node(int v1, int v2, double dist) {
-			this.v1 = v1;
-			this.v2 = v2;
-			this.dist = dist;
-		}
+    static class Node implements Comparable<Node> {
+        int vertex;
+        double dist;
 
-		@Override
-		public int compareTo(Node o) {
-			return Double.compare(this.dist, o.dist);
-		}
-	}
-	static double[][] stars;
-	static int n;
-	static PriorityQueue<Node> heap;
-	static int[] parent, rank;
-	public static void main(String[] args) throws IOException {
-		//System.setIn(new FileInputStream("data/input.txt"));
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		stars = new double[n][2];
-		for (int i = 0; i < n; i++) {
-			stars[i][0] = sc.nextDouble();
-			stars[i][1] = sc.nextDouble();
-		}
-		parent = new int[n];
-		rank = new int[n];
-		for (int i = 0; i < n; i++) {
-			parent[i] = i;
-			rank[i] = 1;
-		}
-		heap = new PriorityQueue<>();
-		for (int i = 0; i < n; i++) {
-			for (int j = i+1; j < n; j++) {
-				heap.add(new Node(i,j,getDistance(i,j)));
-			}
-		}
-		double ans = 0;
-		while (!heap.isEmpty()) {
-			Node cur = heap.poll();
-			if (find(cur.v1) != find(cur.v2)) {
-				union(cur.v1,cur.v2);
-				ans += cur.dist;
-			}
-		}
-		System.out.println(String.format("%.2f", ans));
-	}
-	private static int find(int v) {
-		if (v == parent[v]) return v; 
-			
-		return parent[v] = find(parent[v]);
-	}
-	private static void union(int v1, int v2) {
-		int p1 = find(v1);
-		int p2 = find(v2);
-		if (rank[p1] < rank[p2]) {
-			parent[p1] = p2;
-			rank[p2] += rank[p1];
-		}else {
-			parent[p2] = p1;
-			rank[p1] += rank[p2];
-		}
-		
-	}
-	private static double getDistance(int i, int j) {
-		double x1 = stars[i][0] , y1 = stars[i][1] , x2 = stars[j][0] , y2 = stars[j][1];
-		return Math.hypot(x1-x2, y1-y2);
-	}
+        public Node(int vertex, double dist) {
+            this.vertex = vertex;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Double.compare(this.dist, o.dist);
+        }
+    }
+
+    static double[][] stars;
+    static int n;
+    static double[] dist; // MST에 연결된 노드까지의 최소 거리를 저장하는 배열
+    static boolean[] visited; // MST에 포함되었는지 여부
+
+    public static void main(String[] args) throws IOException {
+       // System.setIn(new FileInputStream("data/input.txt"));
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        stars = new double[n][2];
+        for (int i = 0; i < n; i++) {
+            stars[i][0] = sc.nextDouble();
+            stars[i][1] = sc.nextDouble();
+        }
+
+        dist = new double[n];
+        visited = new boolean[n];
+        
+        for (int i = 0; i < n; i++) {
+            dist[i] = Double.MAX_VALUE;
+        }
+
+        prim(0);
+        
+        double ans = 0;
+        for (double d : dist) {
+            ans += d;
+        }
+        
+        System.out.println(String.format("%.2f", ans));
+    }
+
+    private static void prim(int start) {
+        dist[start] = 0;
+        PriorityQueue<Node> heap = new PriorityQueue<>();
+        heap.offer(new Node(start, 0));
+
+        while (!heap.isEmpty()) {
+            Node currentNode = heap.poll();
+            
+            // 이미 방문한 노드라면 건너뜀
+            if (visited[currentNode.vertex]) {
+                continue;
+            }
+            
+            visited[currentNode.vertex] = true;
+
+            // 현재 노드와 연결된 모든 노드들을 탐색
+            for (int i = 0; i < n; i++) {
+                if (!visited[i]) {
+                    double currentDist = getDistance(currentNode.vertex, i);
+                    
+                    // 새로운 거리가 기존 최소 거리보다 짧다면 갱신
+                    if (currentDist < dist[i]) {
+                        dist[i] = currentDist;
+                        heap.offer(new Node(i, currentDist));
+                    }
+                }
+            }
+        }
+    }
+    
+    private static double getDistance(int i, int j) {
+        double x1 = stars[i][0], y1 = stars[i][1], x2 = stars[j][0], y2 = stars[j][1];
+        return Math.hypot(x1 - x2, y1 - y2);
+    }
 }
