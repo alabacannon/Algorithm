fd292654eba6ad99f7c0c8fd8e61f18cc7c566a8
alabacannon <kim3210542@gmail.com>
2025-09-10 16:41:51 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 689 ms, Memory: 42,388 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,15 +1,16 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
  
 public class Solution {
-    static class Node implements Comparable<Node>{
+    static class Node implements Comparable<Node> {
         int to;
         double cost;
+ 
         public Node(int to, double cost) {
             this.to = to;
             this.cost = cost;
@@ -18,65 +19,67 @@ public class Solution {
         public int compareTo(Node o) {
             return Double.compare(this.cost, o.cost);
         }
-         
     }
+ 
     static int n;
-    static long ans;
+    static double[] minEdge;
     static boolean[] visited;
     static StringBuilder sb;
-    static PriorityQueue<Node> heap;
-    static ArrayList<ArrayList<Node>> graph;
+ 
     public static void main(String[] args) throws IOException {
-//    	BufferedReader br = new BufferedReader(new FileReader("data/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new FileReader("data/input.txt"));
         sb = new StringBuilder();
-        StringTokenizer st;
         int t = Integer.parseInt(br.readLine());
+ 
         for (int test_case = 1; test_case <= t; test_case++) {
-            ans = 0;
             sb.append("#").append(test_case).append(" ");
             n = Integer.parseInt(br.readLine());
-            int[] xArr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int[] yArr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            
+            int[] xArr = new int[n];
+            int[] yArr = new int[n];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) xArr[i] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) yArr[i] = Integer.parseInt(st.nextToken());
+            
             double e = Double.parseDouble(br.readLine());
-            visited = new boolean[n+1];
-            heap = new PriorityQueue<>();
-            graph = new ArrayList<>();
-            for (int i = 0; i <= n; i++) {
-				graph.add(new ArrayList<>());
-			}
-            for (int i = 1; i <= n; i++) {
-                for (int j = i+1; j <= n; j++) {
-                	double d = getDistance(xArr[i-1],yArr[i-1],xArr[j-1],yArr[j-1]);
-                    graph.get(i).add(new Node(j,d)) ;
-                    graph.get(j).add(new Node(i,d)) ;
-                }
-            }
-            heap.offer(new Node(1,0));
+ 
+            minEdge = new double[n]; 
+            visited = new boolean[n];
+            
+            Arrays.fill(minEdge, Double.MAX_VALUE);
+            
+            minEdge[0] = 0;
+            PriorityQueue<Node> heap = new PriorityQueue<>();
+            heap.offer(new Node(0, 0));
+ 
+            double ans = 0;
+            int count = 0;
+ 
             while (!heap.isEmpty()) {
                 Node cur = heap.poll();
-                int to = cur.to;
-                double cost = cur.cost;
-                if (visited[to]) continue; 
-				visited[to] = true;
-				ans += cost;
-                for (Node next : graph.get(to)) {
-                	if (visited[next.to]) continue;
-                	heap.offer(new Node(next.to, next.cost));
-				}
+                if (visited[cur.to]) continue;
+                visited[cur.to] = true;
+                ans += cur.cost;
+                for (int i = 0; i < n; i++) {
+                    if (!visited[i]) {
+                        double distance = getDistance(xArr[cur.to], yArr[cur.to], xArr[i], yArr[i]);
+                        if (distance < minEdge[i]) {
+                            minEdge[i] = distance;
+                            heap.offer(new Node(i, distance));
+                        }
+                    }
+                }
             }
-             
+ 
             sb.append(Math.round(ans * e)).append("\n");
         }
         System.out.println(sb.toString());
         br.close();
     }
-     
  
- 
-    private static double getDistance(int a, int b, int c, int d) {
-        long dx = (long) (a - c);
-        long dy = (long) (b - d);
-        return (double) (dx * dx + dy * dy);
+    private static double getDistance(long x1, long y1, long x2, long y2) {
+        return Math.pow(Math.hypot(x2-x1, y2-y1), 2);
     }
 }
