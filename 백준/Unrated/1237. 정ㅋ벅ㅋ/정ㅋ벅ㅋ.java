bcdad5008e9c6acadac90c44fa297d49d7625529
alabacannon <kim3210542@gmail.com>
2025-09-28 22:50:58 +0900

    [Unrated] Title: 정ㅋ벅ㅋ, Time: 100 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Unrated/1237. 정ㅋ벅ㅋ/정ㅋ벅ㅋ.java
@@ -0,0 +1,10 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("문제의 정답");
+
+    }
+}
