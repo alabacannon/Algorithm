c873ec402577279722b9a14a7822f303a38cf492
alabacannon <kim3210542@gmail.com>
2025-10-27 14:47:05 +0900

    [Silver III] Title: 개미, Time: 64 ms, Memory: 11572 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int p = Integer.parseInt(st.nextToken());
+		int q = Integer.parseInt(st.nextToken());
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		p = (p + t) % (2*w);
+		if(p > w) p  = 2*w - p;
+		
+		q = (q + t) % (2*h);
+		if(q > h) q = 2*h - q;
+		
+		System.out.println(p + " " + q);
+	}
+}
